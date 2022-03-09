7546b200dd6a14eed8fff1a5d3e26dd8a98538d9
JuanPabloGoHaCa <juanpablo.gomezharo@iberopuebla.mx>
2022-03-09 12:37:00 -0600

    version nueva, se envian datos a broker version 1

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,22 +1,33 @@
 from paho.mqtt import client as mqtt_client
 import random
+import time
 
-
-broker = 'broker.hivemq.com'
-port = 1883
-topic = "huber/1"
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
-msg = "prueba"
-
-#conectar
-client = mqtt_client.Client(client_id)
-client.connect(broker, port)
-client.loop_start()
-
-#publicar
-client.publish(topic, msg)
-
-print("Listo")
-
-client.loop_stop()
+#clase que tiene metodos para conectar al broker publico y mandar la informacion que se desee
+class cliente:
+    def __init__(self,broker='broker.hivemq.com',port=1883,topic="huber/1",client_id=f'python-mqtt-{random.randint(0, 1000)}'):
+        self.broker=broker
+        self.port=port
+        self.topic=topic
+        self.client_id=client_id
+    def get_broker(self):
+        return self.broker
+    def get_port(self):
+        return self.port
+    def get_topic(self):
+        return self.topic
+    def get_id(self):
+        return self.client_id
+    def set_msg(self,msg):
+        self.msg=msg
+    def get_msg(self):
+        return self.msg
+    def connect_client(self, list):
+        #msg = "prueba"
+        #conectar
+        client = mqtt_client.Client(self.get_id())
+        client.connect(self.get_broker(), self.get_port())
+        client.loop_start()
+        #publicar
+        for info in list:
+            client.publish(self.get_topic(),str(info["coordenadas"][0][0]))
+        client.loop_stop()

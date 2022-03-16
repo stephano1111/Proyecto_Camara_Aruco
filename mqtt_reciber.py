71345b15b32ee832590e6e102ee3ebc793a44af9
JuanPabloGoHaCa <juanpablo.gomezharo@iberopuebla.mx>
2022-03-16 09:17:34 -0600

    Nuevos cambios

--- mqtt_reciber.py
+++ mqtt_reciber.py
@@ -1,12 +1,19 @@
-from paho.mqtt import client as mqtt_client
-import random
+import paho.mqtt.client as mqtt
 
-broker = 'broker.hivemq.com'
-port = 1883
-topic = "steph/1"
+# This is the Subscriber
 
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+  client.subscribe("huber/#")
 
-client = mqtt_client.Client(client_id)
-client.connect(broker, port)
+def on_message(client, userdata, msg):
+  print(msg.topic+" "+str(msg.payload))
+    
+client = mqtt.Client()
+client.connect("broker.hivemq.com",1883,60)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.loop_forever()
 
